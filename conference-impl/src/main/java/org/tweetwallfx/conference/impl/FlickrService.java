f983362ae9a844771c36830cc2c8c0d3811613a6
Martin Klähn <mklaehn@users.noreply.github.com>
2025-10-09 06:55:20 +0200

    Loading Photos for Photoset in parallel and add EXIF retrieval function (#57)

--- conference-impl/src/main/java/org/tweetwallfx/conference/impl/FlickrService.java
+++ conference-impl/src/main/java/org/tweetwallfx/conference/impl/FlickrService.java
@@ -26,14 +26,16 @@ package org.tweetwallfx.conference.impl;
 import com.flickr4java.flickr.Flickr;
 import com.flickr4java.flickr.FlickrException;
 import com.flickr4java.flickr.REST;
+import com.flickr4java.flickr.photos.Exif;
 import com.flickr4java.flickr.photos.Extras;
 import com.flickr4java.flickr.photos.Photo;
+import com.flickr4java.flickr.photos.PhotosInterface;
 import com.flickr4java.flickr.photosets.Photoset;
 import com.flickr4java.flickr.photosets.PhotosetsInterface;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,16 +48,18 @@ public class FlickrService {
     private static final Logger LOG = LoggerFactory.getLogger(FlickrService.class);
     private static final FlickrSettings FLICKR_SETTINGS = Configuration.getInstance()
             .getConfigTyped(FlickrSettings.CONFIG_KEY, FlickrSettings.class);
-    private final PhotosetsInterface photosInterface;
+    private final PhotosInterface photosInterface;
+    private final PhotosetsInterface photosetsInterface;
     private final ExpiringValue<List<Photoset>> allPhotosets
             = new ExpiringValue<>(this::loadAllPhotosets, Duration.ofSeconds(60));
 
     public FlickrService() {
-        photosInterface = new Flickr(
+        final Flickr flickr = new Flickr(
                 FLICKR_SETTINGS.apiKey(),
                 FLICKR_SETTINGS.apiSecret(),
-                new REST()
-        ).getPhotosetsInterface();
+                new REST());
+        photosetsInterface = flickr.getPhotosetsInterface();
+        photosInterface = flickr.getPhotosInterface();
     }
 
     /**
@@ -71,7 +75,7 @@ public class FlickrService {
         LOG.debug("Request to get all Flickr albums");
 
         try {
-            return List.copyOf(photosInterface.getList(FLICKR_SETTINGS.userId()).getPhotosets());
+            return List.copyOf(photosetsInterface.getList(FLICKR_SETTINGS.userId()).getPhotosets());
         } catch (FlickrException e) {
             throw new RuntimeException("Something went wrong while retrieving the photosets", e);
         }
@@ -81,32 +85,44 @@ public class FlickrService {
      * Get all photos for a specific photoset.
      *
      * @param photoset the album
+     *
+     * @return the list of loaded Photo instances
      */
     public List<Photo> loadAllPhotos(final Photoset photoset) {
         LOG.debug("Request to get [{}] Flickr photos for photoset '{}'", photoset.getPhotoCount(), photoset.getTitle());
+        final int totalItems = photoset.getPhotoCount();
+        final int totalPages = totalItems / TOTAL_PHOTOS_PER_PAGE;
 
-        List<Photo> photos = new ArrayList<>();
-        int totalItems = photoset.getPhotoCount();
+        return IntStream.rangeClosed(1, totalPages)
+                .parallel()
+                .mapToObj(page -> {
+                    try {
+                        return photosetsInterface.getPhotos(
+                                photoset.getId(),
+                                Extras.ALL_EXTRAS,
+                                Flickr.PRIVACY_LEVEL_NO_FILTER,
+                                TOTAL_PHOTOS_PER_PAGE,
+                                page);
+                    } catch (FlickrException fe) {
+                        throw new RuntimeException("Failed loding page " + page + " of photoset " + photoset.getId(), fe);
+                    }
+                })
+                .flatMap(List::stream)
+                .toList();
+    }
 
+    /**
+     * Gets all EXIF informartion for the given photo.
+     *
+     * @param photo the photo
+     *
+     * @return the loaded EXIF data
+     */
+    public List<Exif> getExif(final Photo photo) {
         try {
-            for (int i = 0; i < totalItems; i += TOTAL_PHOTOS_PER_PAGE) {
-                // Calculate the current page number and the number of items to retrieve on this iteration
-                int currentPage = i / TOTAL_PHOTOS_PER_PAGE + 1;
-                int itemsToRetrieve = Math.min(TOTAL_PHOTOS_PER_PAGE, totalItems - i);
-
-                // Retrieve the items for the current page
-                LOG.info("Requesting {} items to retrieve from page {} for photoset {}", itemsToRetrieve, currentPage, photoset.getTitle());
-
-                photos.addAll(photosInterface.getPhotos(photoset.getId(),
-                        Extras.ALL_EXTRAS,
-                        Flickr.PRIVACY_LEVEL_NO_FILTER,
-                        TOTAL_PHOTOS_PER_PAGE,
-                        currentPage));
-            }
+            return List.copyOf(photosInterface.getExif(photo.getId(), null));
         } catch (FlickrException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Something went wrong while retrieving the exif", e);
         }
-
-        return photos;
     }
 }
