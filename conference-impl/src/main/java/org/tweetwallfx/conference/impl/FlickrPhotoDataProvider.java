081a8859da11fc51de6466531e8405a879af6314
Martin Klähn <mklaehn@users.noreply.github.com>
2025-10-09 06:56:28 +0200

    Switching to dateTaken for ImageStore sorting as datePosted appears to (#55)
    be the upload time and dateAdded is primarily absent.

--- conference-impl/src/main/java/org/tweetwallfx/conference/impl/FlickrPhotoDataProvider.java
+++ conference-impl/src/main/java/org/tweetwallfx/conference/impl/FlickrPhotoDataProvider.java
@@ -105,7 +105,7 @@ public class FlickrPhotoDataProvider
                         s.getSource(),
                         uc -> add(
                                 uc,
-                                date(photo.getDatePosted()),
+                                date(photo.getDateTaken()),
                                 additionalData)));
             }
         }
